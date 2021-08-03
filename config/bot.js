cad957ee10494bdd8f92b9391a0becd0aabb7e3f
Glitch (new-bot-ofc) <none>
2021-08-03 08:24:19 +0000

    🗻🎍 Updated with Glitch

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODQ1MzQ5MTY3NzgyMTY2NTg5.YKfqww.0iDxqowJ8PTW1LFcDdaT5JMadNU',
+        prefix: 'p!',
+        activity: 'PortalLands OFC BOT By Slout#8508',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
